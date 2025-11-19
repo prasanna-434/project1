4c3e8444a4e75f33e8b782dfd6bd7ec13e8bc349
prasanna-434 <prasannachanda434@gmail.com>
2025-11-19 21:18:27 +0530

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,17 @@
+
+ function kgstopounds()
+ {
+    let kgs=document.getElementById("kgsvalue").value;
+let pounds=kgs*2.2026;
+ console.log(pounds)
+ let result=document.getElementById("rsvalue");
+ result.innerHTML=pounds
+ }
+ function kmtomiles()
+ {
+    let km=document.getElementById("kmvalue").value;
+let miles=km*0.621371
+console.log(miles)
+let Result=document.getElementById("Resultvalue");
+Resultsult.innerHTML=miles
+ }
